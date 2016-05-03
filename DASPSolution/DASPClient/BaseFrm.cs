5106a4a39d51f3c0b626e4f3c227b3db25280a0c
qlquincy <271759082@qq.com>
2016-05-03 16:14:57 +0800

    注释，图片动态添加，无法预览的问题

--- DASPSolution/DASPClient/BaseFrm.cs
+++ DASPSolution/DASPClient/BaseFrm.cs
@@ -17,7 +17,7 @@ namespace DASPClient
             :base()
         {
             InitializeComponent();
-            this.Icon = Icon.FromHandle(((Bitmap)ResourceDynamicReaderUtils.DynamicLoadResourceByResourceName("logo.png")).GetHicon());
+            //this.Icon = Icon.FromHandle(((Bitmap)ResourceDynamicReaderUtils.DynamicLoadResourceByResourceName("logo.png")).GetHicon());
         }
     }
 }
